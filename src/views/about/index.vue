73d97111baa9f784f2abf017f4c1dba9e1228db9
use2 <user2@163.com>
2016-11-05 00:02:57 +0800

    event.preventDefault()

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,17 +1,19 @@
 <template>
     <div id="about">
         <slidebar :title="title"></slidebar>
-        <div class="about-block" :class="{'mask':!controllerHide}">
-            <img src="http://tva2.sinaimg.cn/crop.518.65.901.901.180/b2f93831jw8evp8z6co3vj21gx0ts4c2.jpg">
+        <div @click='showQrcode()' class='mask' :class="{'comment-controller-hide': controllerHide}"></div>
+        <div id="about-block" class="about-block">
+            <img src="../../assets/images/common/avatr.jpg">
             <h4>学习Vue，Vue大法好!</h4>
-            <p>by wy</p>
+            <p>by WY</p>
             <p>感谢<a href="https://github.com/gundam1993/zhihu-daily-with-vue">@gundam1993</a></p>
             <p>感谢<a href="https://github.com/axhello">@axhello</a></p>
             <ul>
                 <li>
                     <a href="https://github.com/wanfeiyy/me-vue">项目github地址</a>
                 </li>
-                <li @click= 'showQrcode()' style="font-size:0.4rem;font-weight:bold">欢迎关注公众号,联系我</li>
+
+                <li @click='showQrcode()' style="font-size:0.4rem;font-weight:bold">欢迎关注公众号,联系我</li>
                 <li>喜欢的话</li>
                 <li>给一个star好吗</li>
                 <li style="color: #FF4B84;"
@@ -21,12 +23,12 @@
                 <a v-link="'index'">返回</a>
             </div>
         </div>
-        <div class="qrcode" :class="{'comment-controller-hide': controllerHide}"></div>
+        <div v-el:btn class="qrcode" :class="{'comment-controller-hide': controllerHide}"></div>
     </div>
 </template>
+
 <style lang="scss" scoped>
     .about-block {
-
         width: 100%;
         text-align: center;
         font-size: .6rem;
@@ -81,19 +83,30 @@
         width: 100%;
         height: 100%;
         z-index: 999;
-        top: 40%;
+        top: 0;
         left: 25%;
         background: url("../../assets/images/common/qrcode.png") no-repeat;
         background-size: 50%;
+        transition: all 0.8s ease-in-out;
+        -webkit-transform: translateY(40%);
+        transform: translateY(40%);
     }
+
     .mask {
-        background: rgba(0,0,0,.6);
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.6);
+        z-index: 998;
     }
 
     .comment-controller-hide {
         transform: translateY(100%);
         opacity: 0;
-        transition: all 0.5s ease;
+        transition: all 0.5s ease-in-out;
+
     }
 
 </style>
@@ -110,11 +123,17 @@
             Slidebar
         },
         methods: {
-            showQrcode() {
-                this.controllerHide = false;
+            func() {
+               event.preventDefault();
             },
-
+            showQrcode() {
+                this.controllerHide = !this.controllerHide;
+                if (this.$els.btn.className == 'qrcode comment-controller-hide') {
+                    document.addEventListener('touchmove',this.func)
+                } else {
+                    document.removeEventListener('touchmove',this.func)
+                }
+            },
         }
-
     }
 </script>
